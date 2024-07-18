039b4caf424d96ba9cf281c6300d251dcf8a57e6
Dhiraj Das <23230081+dhiraj-das@users.noreply.github.com>
2024-07-18 12:17:45 +0000

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,25 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
 
-st.title("🎈 My new app")
+# Data ETL
+nifty50_df = pd.read_csv('data/nifty50_tri.csv')
+nifty250_df = pd.read_csv('data/nifty250_tri.csv')
+combined_df = nifty50_df.copy(deep=True)
+combined_df['Smallcap_TRI'] = nifty250_df['Total Returns Index']
+combined_df.index = pd.to_datetime(combined_df['Date'], format='%d %b %Y')
+
+# Code for the GUI of the app goes here
+st.title("📈 Smallcap Investment Strategy")
 st.write(
-    "Let's start building! for help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "An investment strategy for investing into smallcap"
 )
+
+st.dataframe(combined_df)
+
+fig = px.line(combined_df, x='Date', y=['Total Returns Index', 'Smallcap_TRI'], color_discrete_sequence=["#0514C0"], labels={'y': 'TRI'})
+fig.update_layout(xaxis=dict(autorange="reversed"))
+fig.update_layout(title='NIFTY 50 TRI', xaxis_title='Date', yaxis_title='TRI')
+
+st.plotly_chart(fig, use_container_width=True)
